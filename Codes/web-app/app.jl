14183d892f724332966c098670ce1780a29a4e6f
L-Bayrem <160652712+Lamouchi-Bayrem@users.noreply.github.com>
2024-05-11 21:07:31 +0100

    Update app.jl

--- Codes/web-app/app.jl
+++ Codes/web-app/app.jl
@@ -1,17 +1,18 @@
-using GenieFramework
-@genietools
+using Genie
 
 @app begin
     
     @in N::Int32 = 1000
     @in amp::Float32 = 0.25
     @in freq::Int32 = 1
+    @in phase::Float32 = 0.0
+    @in offset::Float32 = 0.0
     
     @out my_sine = PlotData()
 
-    @onchange N, amp, freq begin
+    @onchange N, amp, freq, phase, offset begin
         x = range(0, 1, length=N)
-        y = amp*sin.(2*π*freq*x)
+        y = amp*sin.(2*π*freq*x + phase) + offset
         
         my_sine = PlotData(x=x, 
                            y=y, 
@@ -21,4 +22,3 @@ using GenieFramework
 end
     
 @page("/", "app.jl.html")
-
